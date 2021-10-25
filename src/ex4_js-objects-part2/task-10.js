c067b51c743809c2c1042c6452f1347443ed01d9
babaika20 <69527126+Eliseeev@users.noreply.github.com>
2021-10-26 00:23:07 +0300

    Delete task-10.js

--- src/ex4_js-objects-part2/task-10.js
+++
@@ -1,7 +0,0 @@
-function inverted(string) {
-  const spLine = string.split('');
-  const revMass = spLine.reverse();
-  const resInv = revMass.join('');
-  return resInv;
-}
-module.exports = inverted;
